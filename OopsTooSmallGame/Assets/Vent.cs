ca67be4f8f39e9057626dd7e6d551f1f5cef4128
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-02-20 16:55:36 -0600

    playable

--- OopsTooSmallGame/Assets/Vent.cs
+++ OopsTooSmallGame/Assets/Vent.cs
@@ -16,4 +16,11 @@ public class Vent : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // this will work as a temporary solution for one scene transition, until we have a proper scene manager.
+        Debug.Log("hit vent");
+        SceneManager.LoadScene("Kitchen");
+    }
 }
