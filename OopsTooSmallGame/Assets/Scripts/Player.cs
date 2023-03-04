5052de60229d0f0a70031f07ad98fa54b8b74732
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-03-04 16:56:19 -0600

    wall jump is working

--- OopsTooSmallGame/Assets/Scripts/Player.cs
+++ OopsTooSmallGame/Assets/Scripts/Player.cs
@@ -29,10 +29,14 @@ public class Player : MonoBehaviour
     private bool jumpPressed;
     private bool canWallJump;
     private float horizontal;
+    private bool freeFalling;
     //private int health;
     private Vector2 wallNormal;
     private Vector3 startPos;
     private bool jumped = false;
+
+    public bool canJump;
+    public float jumpCooldown = 1f;
 
     private Transform wallTransform;
 
@@ -44,6 +48,8 @@ public class Player : MonoBehaviour
         //health = 3;
         audioSource = GetComponent<AudioSource>();
         jumpSound = Resources.Load<AudioClip>("Sounds/jump");
+        canJump = true;
+        freeFalling = false;
     }
 
     // Update is called once per frame -- used to detect key presses
@@ -57,12 +63,16 @@ public class Player : MonoBehaviour
                                             wallCheckRadius,
                                             groundLayer);
         bool upPressed = Input.GetKey("up") || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W);
+        
+        if (isGrounded) {
+            freeFalling = false;
+        }
+
         if (upPressed && (isGrounded || isWalled))
         {
             jumpPressed = true;
         }
-         //If lives being lost needs to be implemented, add it within this
-         //if statement
+         
         if(this.gameObject.transform.position.y < deathLowBound)
         {
             if (health.Death())
@@ -79,8 +89,9 @@ public class Player : MonoBehaviour
         if (!isWallJumping)
         {
             TryMove();
+            TryJump();
+
         }
-        TryJump();
         TryWallBehavior();
     }
 
@@ -94,42 +105,91 @@ public class Player : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
-        rigbod.velocity = new Vector2(horizontal * walkSpeed, rigbod.velocity.y);
+
+        if (freeFalling && horizontal == 0) {
+            rigbod.velocity = new Vector2(rigbod.velocity.x, rigbod.velocity.y);
+
+        }
+
+        else {
+            rigbod.velocity = new Vector2(horizontal * walkSpeed, rigbod.velocity.y);
+
+        }
 
         //TryJump();
     }
 
     private void TryJump()
     {
-        if (jumpPressed && isGrounded)
-        {
-            rigbod.velocity = new Vector2(rigbod.velocity.x, jumpPower);
+
+        if (!canJump) {
             jumpPressed = false;
-            jumped = true;
+            return;
         }
-        
-        if (jumpPressed && isWalled)
-        {
-            isWallJumping = true;
-            float modifier = wallJumpPower;
-            //RIGHT WALL:
-            if (wallTransform.position.x > this.transform.position.x)
+
+        else { 
+
+            if (jumpPressed && isGrounded)
             {
-                Debug.Log("wall is on the right");
-                modifier = -modifier;
+                rigbod.velocity = new Vector2(rigbod.velocity.x, jumpPower);
+                jumpPressed = false;
+                jumped = true;
+
+                // Debug.Log("jump from ground");
+                // canJump = false;
+                StartCoroutine(StartCooldown());
+
+                return;
             }
-            rigbod.AddForce(new Vector2(modifier, jumpPower * 15));
-            jumpPressed = false;
-            Invoke("DontWallJump", wjTime);
+        
+            if (jumpPressed && isWalled)
+            {
 
-            jumped = true;
+                
+                isWallJumping = true;
+                float modifier = wallJumpPower;
+                //RIGHT WALL:
+                if (wallTransform.position.x > this.transform.position.x)
+                {
+                    // Debug.Log("wall is on the right");
+                    modifier = -modifier;
+                }
+                // rigbod.AddForce(new Vector2(modifier, jumpPower * 10));
+                rigbod.velocity = new Vector2(modifier, jumpPower);
+                freeFalling = true;
+
+                // jumpPressed = false;
+                Invoke("DontWallJump", wjTime);
+
+                jumped = true;
+                canJump = false;
+                // Debug.Log("jump from wall");
+                StartCoroutine(StartCooldown());
+
+                return;
+
+            }
+
+            if (jumped) {
+                audioSource.PlayOneShot(jumpSound);
+                jumped = false;
+
+            }
+
+            
+
+
         }
+        return;
+        
+    }
 
-        if (jumped) {
-            audioSource.PlayOneShot(jumpSound);
-            jumped = false;
-
-        }
+    IEnumerator StartCooldown() {
+        canJump = false;
+        Debug.Log("CoroutineExample started at " + Time.time.ToString() + "s");         
+        yield return new WaitForSeconds(jumpCooldown);
+        Debug.Log("Coroutine Iteration Successful at " + Time.time.ToString() + "s");   
+        canJump = true;
     }
 
     private void TryWallBehavior()
@@ -148,7 +208,7 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("collision detected");
+        // Debug.Log("collision detected");
         if (isWalled)
         {
             wallTransform = collision.collider.GetComponent<Transform>();
