d563fefc3c70b7f5e63bf08688bf76b3bbe9b8b9
rjruddy <80654885+rjruddy@users.noreply.github.com>
2023-03-06 13:40:11 -0600

    basement

--- OopsTooSmallGame/Assets/Scripts/Player.cs
+++ OopsTooSmallGame/Assets/Scripts/Player.cs
@@ -194,6 +194,8 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        wallTransform = collision.collider.GetComponent<Transform>();
+        /*
         if (isWalled)
         {
             wallTransform = collision.collider.GetComponent<Transform>();
@@ -204,8 +206,9 @@ public class Player : MonoBehaviour
                 Debug.Log("platform collision detected");
                 wall = collision.collider.GetComponent<GameObject>();
             }
-            */
+            
         }
+        */
 
 
     }
@@ -220,6 +223,9 @@ public class Player : MonoBehaviour
         {
             evmanScript.CollectedKey();
             Destroy(collision.gameObject);
+        } else if (collision.gameObject.CompareTag("boss-head"))
+        {
+            SceneManager.LoadScene("WinScreen");
         }
     }
    
